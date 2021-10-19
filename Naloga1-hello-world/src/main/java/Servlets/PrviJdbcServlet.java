2fd00b6ac8ce2e462e5997cccabaeea32573a925
Gregor Bučar <gregor.bucar@gmail.com>
2021-10-19 13:27:41 +0200

    Dodan Servlet

---
+++ Naloga1-hello-world/src/main/java/Servlets/PrviJdbcServlet.java
@@ -0,0 +1,17 @@
+package Servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/servlet")
+public class PrviJdbcServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // implementacija
+
+    }
+}
