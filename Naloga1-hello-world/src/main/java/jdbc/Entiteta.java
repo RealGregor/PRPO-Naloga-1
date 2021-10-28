20d6bdc922a1a0d430fa6e58241a139437de96cc
ziga7631 <zd7631@student.uni-lj.si>
2021-10-28 15:57:42 +0200

    RELEASE-0.1

--- Naloga1-hello-world/src/main/java/jdbc/Entiteta.java
+++ Naloga1-hello-world/src/main/java/jdbc/Entiteta.java
@@ -3,7 +3,7 @@ package jdbc;
 import java.io.Serializable;
 
 public abstract class Entiteta implements Serializable {
-    public int id;
+    private int id;
 
     public int getId() {
         return id;
